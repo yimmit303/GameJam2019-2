16da4faf130559e8409b24c7f53582c8d43fcae8
cheapscate98 <35314966+cheapscate98@users.noreply.github.com>
2019-11-09 16:04:16 -0500

    First puzzle finished
    Doors now work

--- Scripts/Opened Door.gd
+++ Scripts/Opened Door.gd
@@ -4,18 +4,21 @@ extends Node2D
 # var a = 2
 # var b = "text"
 
+var sBody
+var cRect
+var pos
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	self.visible = false
-	var child = get_node("StaticBody2D/CollisionShape2D")
-	child.disabled = true
-	pass # Replace with function body.
+	sBody = get_node("StaticBody2D/CollisionShape2D")
+	cRect = get_node("StaticBody2D/ColorRect")
+	pos = self.position
+	self.position = Vector2(1000000000,1000000000)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
 func open():
-	self.visible = true
-	var child = get_node("StaticBody2D/CollisionShape2D")
-	child.disabled = false
+	self.position = pos
+	pass
