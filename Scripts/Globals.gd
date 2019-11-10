c764648285ffe25bfa578814e186dc41de3a4868
cheapscate98 <35314966+cheapscate98@users.noreply.github.com>
2019-11-09 22:34:54 -0500

    Various Changes
    Added art assets for buttons
    Resized background images
    Added Circles Node to every Level
    Probably other changes

--- Scripts/Globals.gd
+++ Scripts/Globals.gd
@@ -7,7 +7,7 @@ var just_started
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	just_started = true
+	just_started = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
