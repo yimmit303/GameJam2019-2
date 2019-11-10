0f0f0ee9ec1980fdeef2302ecae29fd759850154
yimmit303 <timsteiner303@gmail.com>
2019-11-09 20:33:17 -0500

    Create Globals.gd

---
+++ Scripts/Globals.gd
@@ -0,0 +1,14 @@
+extends Node
+
+# Declare member variables here. Examples:
+# var a = 2
+
+var just_started
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	just_started = true
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
