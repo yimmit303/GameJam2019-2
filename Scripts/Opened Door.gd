109922df8e75c8bec67c9b62460d54efbda2b68e
cheapscate98 <35314966+cheapscate98@users.noreply.github.com>
2019-11-10 13:45:01 -0500

    Open Door Outline

--- Scripts/Opened Door.gd
+++ Scripts/Opened Door.gd
@@ -10,19 +10,19 @@ var pos
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	sBody = get_node("StaticBody2D/CollisionShape2D")
-	cRect = get_node("StaticBody2D/ColorRect")
-	pos = self.position
-	self.position = Vector2(1000000000,1000000000)
+	sBody = get_node("StaticBody2D")
+	cRect = get_node("ColorRect")
+	pos = sBody.position
+	sBody.position = Vector2(1000000000,1000000000)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
-func open():
-	self.position = pos
-	pass
-
 func close():
-	self.position = pos
+	sBody.position = pos
+	cRect.color = Color(.5,1,.5, 1)
 	pass # Replace with function body.
+
+func _draw():
+	draw_rect(cRect.get_rect(), Color(.5,1,.5,.5), false)
